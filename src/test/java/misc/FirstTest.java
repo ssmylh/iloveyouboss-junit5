0bb9d56d53233b117209bfb9d20061e54bd1c385
ssmylh <ssmylh@gmail.com>
2016-12-10 16:57:57 +0900

    Use wildcard for import static

--- src/test/java/misc/FirstTest.java
+++ src/test/java/misc/FirstTest.java
@@ -2,7 +2,7 @@ package misc;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FirstTest {
 
