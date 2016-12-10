21f2c615c89865bbbc088e724b6590fb5ed95a2e
ssmylh <ssmylh@gmail.com>
2016-12-10 15:08:04 +0900

    First test

---
+++ src/test/java/misc/FirstTest.java
@@ -0,0 +1,13 @@
+package misc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
