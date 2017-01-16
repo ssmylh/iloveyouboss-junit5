40a20577bbdbfae45e7f58df355fa017cfe8a63b
ssmylh <ssmylh@gmail.com>
2017-01-16 20:51:27 +0900

    Refactor ScoreCollectionTest

--- src/test/java/iloveyouboss/ScoreCollectionTest.java
+++ src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -1,14 +1,21 @@
 package iloveyouboss;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ScoreCollectionTest {
+    ScoreCollection collection;
+
+    @BeforeEach
+    void create() {
+        collection = new ScoreCollection();
+    }
+
 
     @Test
     void answersArithmeticMeanOfTwoNumbers() {
-        ScoreCollection collection = new ScoreCollection();
         collection.add(() -> 5);
         collection.add(() -> 7);
 
