89617c383a99d0013b3a435783d5daa9b746d1e3
ssmylh <ssmylh@gmail.com>
2017-01-16 22:11:46 +0900

    Chapter 6

--- src/test/java/iloveyouboss/ProfileTest.java
+++ src/test/java/iloveyouboss/ProfileTest.java
@@ -3,6 +3,10 @@ package iloveyouboss;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.time.Duration.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class ProfileTest {
@@ -31,5 +35,42 @@ public class ProfileTest {
         criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
         assertThat(profile.matches(criteria)).isTrue();
+    }
+
+    int[] ids(List<Answer> answers) {
+        return answers.stream()
+                .mapToInt(a -> a.getQuestion().getId()).toArray();
+    }
+
+    @Test
+    void findsAnswersBasedOnPredicate() {
+        profile.add(new Answer(new BooleanQuestion(1, "1"), Bool.FALSE));
+        profile.add(new Answer(new PercentileQuestion(2, "2", new String[]{}), 0));
+        profile.add(new Answer(new PercentileQuestion(3, "3", new String[]{}), 0));
+
+        List<Answer> answers = profile.find(a -> a.getQuestion().getClass() == PercentileQuestion.class);
+        assertThat(ids(answers)).containsExactly(2, 3);
+
+        List<Answer> answersComplement = profile.find(a -> a.getQuestion().getClass() != PercentileQuestion.class);
+        List<Answer> allAnswers = new ArrayList<>();
+        allAnswers.addAll(answersComplement);
+        allAnswers.addAll(answers);
+        assertThat(ids(allAnswers)).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    void findAnswers() {
+        int dataSize = 5000;
+        for (int i = 0; i < dataSize; i++) {
+            profile.add(new Answer(new BooleanQuestion(i, String.valueOf(i)), Bool.FALSE));
+        }
+        profile.add(new Answer(new PercentileQuestion(dataSize, String.valueOf(dataSize), new String[]{}), 0));
+
+        org.junit.jupiter.api.Assertions.assertTimeout(ofMillis(1000), () -> {
+            int numberOfTimes = 1000;
+            for (int i = 0; i < numberOfTimes; i++) {
+                profile.find(a -> a.getQuestion().getClass() == PercentileQuestion.class);
+            }
+        });
     }
 }
--- src/test/java/iloveyouboss/ScoreCollectionTest.java
+++ src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -21,4 +21,26 @@ public class ScoreCollectionTest {
 
         assertThat(collection.arithmeticMean()).isEqualTo(6);
     }
+
+    @Test
+    void throwsExceptionWhenAddingNull() {
+        Throwable iae = catchThrowable(() -> {
+            collection.add(null);
+        });
+
+        assertThat(iae).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void answersZeroWhenNoElementsAdded() {
+        assertThat(collection.arithmeticMean()).isEqualTo(0);
+    }
+
+    @Test
+    void dealsWithIntegerOverflow() {
+        collection.add(() -> Integer.MAX_VALUE);
+        collection.add(() -> 1);
+
+        assertThat(collection.arithmeticMean()).isEqualTo(1073741824);
+    }
 }
