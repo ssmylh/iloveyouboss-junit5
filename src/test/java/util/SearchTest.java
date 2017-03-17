90b7fa2ce34e7fd2fbb80a7fd7ef6720b07d9dd4
ssmylh <ssmylh@gmail.com>
2017-03-17 20:57:51 +0900

    Chapter 11.9

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -20,8 +20,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -45,30 +43,25 @@ public class SearchTest {
     @Disabled
     @Test
     public void returnsMatchesShowingContextWhenSearchStringInContent() throws Exception {
-        stream = streamOn("There are certain queer times and occasions "
-                + "in this strange mixed affair we call life when a man "
-                + "takes this whole universe for a vast practical joke, "
-                + "though the wit thereof he but dimly discerns, and more "
-                + "than suspects that the joke is at nobody's expense but "
-                + "his own.");
-        Search search = new Search(stream, "practical joke", A_TITLE);
+        stream = streamOn("rest of text here"
+                + "1234567890search term1234567890" + "more rest of text");
+        Search search = new Search(stream, "search term", A_TITLE);
+
         search.setSurroundingCharacterCount(10);
 
         search.execute();
 
         List<Match> matches = search.getMatches();
         assertThat(matches.toArray(new Match[matches.size()])).containsMatches(new Match[]{
-                new Match(A_TITLE, "practical joke", "or a vast practical joke, though t")
+                new Match(A_TITLE, "search term", "1234567890search term1234567890")
         });
     }
 
     @Disabled
     @Test
     public void noMatchesReturnedWhenSearchStringNotInContent() throws Exception {
-        URLConnection connection =
-                new URL("http://bit.ly/15sYPA7").openConnection();
-        stream = connection.getInputStream();
-        Search search = new Search(stream, "smelt", A_TITLE);
+        stream = streamOn("any text");
+        Search search = new Search(stream, "text that doesn't match", A_TITLE);
 
         search.execute();
 
