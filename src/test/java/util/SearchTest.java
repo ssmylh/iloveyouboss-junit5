a6e51c228c1dc726c97d9625eceba3bc60d56118
ssmylh <ssmylh@gmail.com>
2017-03-17 20:49:29 +0900

    Chapter 11.7

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -13,6 +13,8 @@ package util;
 // http://www.gutenberg.org/cache/epub/2701/pg2701.txt
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -28,26 +30,34 @@ import static util.SearchTestAssertions.*;
 
 public class SearchTest {
     private static final String A_TITLE = "1";
+    private InputStream stream;
+
+    @BeforeEach
+    public void turnOffLogging() {
+        Search.LOGGER.setLevel(Level.OFF);
+    }
+
+    @AfterEach
+    public void closeResources() throws Exception {
+        stream.close();
+    }
 
     @Disabled
     @Test
     public void returnsMatchesShowingContextWhenSearchStringInContent() throws Exception {
-        InputStream stream = streamOn("There are certain queer times and occasions "
+        stream = streamOn("There are certain queer times and occasions "
                 + "in this strange mixed affair we call life when a man "
                 + "takes this whole universe for a vast practical joke, "
                 + "though the wit thereof he but dimly discerns, and more "
                 + "than suspects that the joke is at nobody's expense but "
                 + "his own.");
         Search search = new Search(stream, "practical joke", A_TITLE);
-        Search.LOGGER.setLevel(Level.OFF);
         search.setSurroundingCharacterCount(10);
         search.execute();
-        assertThat(search.errored()).isFalse();
         List<Match> matches = search.getMatches();
         assertThat(matches.toArray(new Match[matches.size()])).containsMatches(new Match[]{
                 new Match(A_TITLE, "practical joke", "or a vast practical joke, though t")
         });
-        stream.close();
     }
 
     @Disabled
@@ -55,11 +65,10 @@ public class SearchTest {
     public void noMatchesReturnedWhenSearchStringNotInContent() throws Exception {
         URLConnection connection =
                 new URL("http://bit.ly/15sYPA7").openConnection();
-        InputStream stream = connection.getInputStream();
+        stream = connection.getInputStream();
         Search search = new Search(stream, "smelt", A_TITLE);
         search.execute();
         assertThat(search.getMatches()).isEmpty();
-        stream.close();
     }
 
     private InputStream streamOn(String pageContent) {
